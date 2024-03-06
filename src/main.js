b3b4e8a5007e88e254b719bfd95e83188b5d44e6
Šimon Prokopec <prokopec.simon@nvsp.cz>
2024-03-06 12:28:58 +0100

    change to multiple inputs

--- src/main.js
+++ src/main.js
@@ -10,38 +10,46 @@ const proxyConfiguration = await Actor.createProxyConfiguration({
     countryCode: 'CZ',
 });
 
-const { urlTrail, imgId } = input;
-const fullUrl = `https://img-cdn.hltv.org/teamlogo/${imgId}${urlTrail}`;
+const { data } = input;
 
-const crawler = new PuppeteerCrawler({
-    proxyConfiguration,
-    async requestHandler({ page }) {
-        const isPng = fullUrl.includes('.png');
+const processInput = async ({ imgId, urlTrail }) => {
+    const fullUrl = `https://img-cdn.hltv.org/teamlogo/${imgId}${urlTrail}`;
 
-        let imageData;
-        if (isPng) {
-            const [response] = await Promise.all([
-                page.waitForResponse((pageResponse) => pageResponse.url().includes('.png')),
-                page.goto(fullUrl),
-            ]);
-            const buffer = await response.buffer();
-            imageData = Buffer.from(buffer).toString('base64');
-        } else {
-            const svgContent = await page.evaluate(() => {
-                const svgElement = document.querySelector('svg');
-                return svgElement.outerHTML;
+    const crawler = new PuppeteerCrawler({
+        proxyConfiguration,
+        async requestHandler({ page }) {
+            const isPng = fullUrl.includes('.png');
+
+            let imageData;
+            if (isPng) {
+                const [response] = await Promise.all([
+                    page.waitForResponse((pageResponse) => pageResponse.url().includes('.png')),
+                    page.goto(fullUrl),
+                ]);
+                const buffer = await response.buffer();
+                imageData = Buffer.from(buffer).toString('base64');
+            } else {
+                const svgContent = await page.evaluate(() => {
+                    const svgElement = document.querySelector('svg');
+                    return svgElement.outerHTML;
+                });
+
+                imageData = Buffer.from(svgContent).toString('base64');
+            }
+
+            await Actor.pushData({
+                status: 200,
+                data: [{ img: imageData }],
             });
+        },
+    });
 
-            imageData = Buffer.from(svgContent).toString('base64');
-        }
+    await crawler.run([fullUrl]);
+};
 
-        await Actor.pushData({
-            status: 200,
-            data: [{ img: imageData }],
-        });
-    },
-});
-
-await crawler.run([fullUrl]);
+// Process each input in the array
+for (const inputItem of data) {
+    await processInput(inputItem);
+}
 
 await Actor.exit();
